e8efc9c8d2bd6dba031a4067e8d401c08856f954
benrcd <richard.benjamin69@yahoo.fr>
2025-02-27 10:33:01 +0100

    Profile entity + migration done. Also fixed composer.lock regarding laravel version

---
+++ database/migrations/2025_02_27_091734_create_profiles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('lastname',30);
+            $table->string('firstname',20);
+            $table->string('image')->nullable();
+            $table->enum('status', ['inactif', 'en attente', 'actif'])->default('en attente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
