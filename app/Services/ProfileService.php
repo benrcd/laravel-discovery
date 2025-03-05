101b996d768b9d4617feb8f9939e5a56142f6bb7
benrcd <richard.benjamin69@yahoo.fr>
2025-03-05 14:40:59 +0100

    Same for updateProfile

--- app/Services/ProfileService.php
+++ app/Services/ProfileService.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\UploadedFile;
 use Exception;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileService
 {
@@ -26,6 +27,13 @@ class ProfileService
     public function updateProfile(Profile $profile, array $data): Profile
     {
         try {
+            if (isset($data['image']) && $data['image'] instanceof UploadedFile) {
+                if ($profile->image) {
+                    Storage::disk('public')->delete($profile->image);
+                }
+                $imagePath = $data['image']->store('profiles', 'public');
+                $data['image'] = $imagePath;
+            }
             $profile->update($data);
             return $profile;
         } catch (Exception $e) {
